a068b6120f5827a946fd1cdf4045271b1e7cb817
stellanamen <121315996+stellanamen@users.noreply.github.com>
2023-06-13 14:57:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+menuListArray = ["Pizza Vegetariana","pizza de frango","pizza portuguesa","pizza quatro queijos","pizza calabresa","pzza extravaganza"
+                    ];
+
+function getMenu(){
+var htmldata;
+menuListArray.sort();
+for (var i=0;i<menuListArray.length;i++){
+    htmldata=htmldata+ menuListArray[i] + '<br>'
+}
+document.getElementById("displayMenu").innerHTML = htmldata;
+}
+
+function addItem(){
+var htmldata;
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata=""
+for (var i=0;i<menuListArray.length;i++){
+    htmldata=htmldata+ menuListArray[i] + '<br>'
+}
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
+
+
+}
+
+function addTop(){
+var item=document.getElementById(addItem).value;
+menuListArray.push(item);
+addItem();
+}
